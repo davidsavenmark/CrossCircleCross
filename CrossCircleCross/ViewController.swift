e049339644165d664a43046fbab055a84aa07bd3
David <dsinemark@gmail.com>
2020-12-26 16:37:50 +0100

    Initial Commit

---
+++ CrossCircleCross/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CrossCircleCross
+//
+//  Created by David Sävenmark on 2020-12-26.
+//  Copyright © 2020 David Sävenmark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
