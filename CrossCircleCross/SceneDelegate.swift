05d7987ffe65377e68513c3df0b71a5f373131b7
David <dsinemark@gmail.com>
2020-12-26 16:46:33 +0100

    New Xcode project for the tic-tac-toe game.

---
+++ CrossCircleCross/SceneDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  SceneDelegate.swift
+//  CrossCircleCross
+//
+//  Created by David Sävenmark on 2020-12-26.
+//  Copyright © 2020 David Sävenmark. All rights reserved.
+//
+
+import Foundation
