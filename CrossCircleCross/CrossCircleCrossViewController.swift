2f23fde6dba7c8b68e707dab5d68c6a75296e7be
David <dsinemark@gmail.com>
2021-01-15 16:04:00 +0100

    Latest commit

--- CrossCircleCross/CrossCircleCrossViewController.swift
+++ CrossCircleCross/CrossCircleCrossViewController.swift
@@ -13,11 +13,11 @@ class CrossCircleCrossViewController: UIViewController {
     
     let winningCombinations: Array = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6], [0, 4, 8]] // All possible win combinations
     
-    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0,] // All 0 means that no one has made their move yet,
+    var gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]    // All 0 means that no one has made their move yet,
                                                   // if Cross makes a move it will be a 1 instead of 0
                                                  // if Circle makes a move it will be a 2 instea of 0
     
-    var activePlayer = 1 // 1 is Cross and 2 is Circle
+    var activePlayer = 1 // 1 is Cross and 2 is Circle, Cross always starts.
     var gameIsActive =  true
     
    
@@ -32,7 +32,7 @@ class CrossCircleCrossViewController: UIViewController {
     {
         if(gameState[sender.tag-1] == 0 && gameIsActive == true)
         {
-            gameState[sender.tag-1] = activePlayer
+            gameState[sender.tag-1] = activePlayer // checks if gameState has a 1 or a 2 in the list.
             label_turn.text = "Cross turn"
             
             
@@ -64,7 +64,12 @@ class CrossCircleCrossViewController: UIViewController {
         for combination in winningCombinations
         {
             
-            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]] // Checks if the gameStates values matches the numbers in the different combinations for example: combination [1, 4, 7] = [0, 1, 0, 0, 4, 0, 0, 7, 0]
+             // Checks if the gameStates values matches the numbers in the different combinations for example: combination [1, 4, 7] = [0, 1, 0, 0, 4, 0, 0, 7, 0]
+             // The statement checks first that the activePlayer is not zero then checks whether the second element is equal to the first and then
+            // whether the third element is equal to the second. If this condition is true, then either activePlayer 1 or 2 has won. 
+            if gameState[combination[0]] != 0 && gameState[combination[0]] == gameState[combination[1]] && gameState[combination[1]] == gameState[combination[2]]
+                
+               
                 
                 
             
@@ -74,7 +79,7 @@ class CrossCircleCrossViewController: UIViewController {
                 label_turn.isHidden = true
                 
                 
-                if gameState[combination[0]] == 1
+                if gameState[combination[0]] == 1 // checks who has won
                 {
                    
                     label.text = "CROSS WINS!"
@@ -106,13 +111,13 @@ class CrossCircleCrossViewController: UIViewController {
             
             {
                 gameIsActive = true
-                break
+                break // breaks the loop
                 
             }
             
         }
         
-        if gameIsActive == false  // If all pieces have been laied but game is till active it is a draw.
+        if gameIsActive == false  //  Displays if it is a draw when all pieces have been layed but no winning combination has been triggered.
         {
             label.text = "IT IS A DRAW!"
             label.isHidden = false
@@ -130,7 +135,7 @@ class CrossCircleCrossViewController: UIViewController {
     @IBAction func playAgain(_ sender: AnyObject)
     
     {
-        gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0,]
+        gameState = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         gameIsActive = true
         activePlayer = 1
         
@@ -140,7 +145,7 @@ class CrossCircleCrossViewController: UIViewController {
         
         for i in 1...9
         {
-            let button = view.viewWithTag(i) as! UIButton
+            let button = view.viewWithTag(i) as! UIButton // Resets the gameboard by converting the view with the tags ( i ) to a UIButton in order to set the image to nil.
             button.setImage(nil, for: UIControl.State())
         }
     }
